3297a4606c801e2736c0d0649031b1cfa88a5434
Prabin Hada <phada@Prabins-MacBook-Pro.local>
2022-01-15 10:05:08 -0500

    quick sort

--- binary_tree.py
+++ binary_tree.py
@@ -53,6 +53,60 @@ class BinarySearchTree:
             current_node = current_node.right
         return current_node
 
+    def breadth_first_search(self):
+        current_node = self.root
+        queue = []
+        results = []
+        queue.append(current_node)
+
+        while len(queue) > 0:
+            current_node = queue.pop(0)
+            results.append(current_node.value)
+            if current_node.left is not None:
+                queue.append(current_node.left)
+            if current_node.right is not None:
+                queue.append(current_node.right)
+        return results
+
+    def depth_first_pre_order(self):
+        results = []
+
+        def traverse(current_node):
+            results.append(current_node.value)
+            if current_node.left is not None:
+                traverse(current_node.left)
+            if current_node.right is not None:
+                traverse(current_node.right)
+
+        traverse(self.root)
+        return results
+
+    def depth_first_post_order(self):
+        results = []
+
+        def traverse(current_node):
+            if current_node.left is not None:
+                traverse(current_node.left)
+            if current_node.right is not None:
+                traverse(current_node.right)
+            results.append(current_node.value)
+
+        traverse(self.root)
+        return results
+
+    def depth_first_inorder(self):
+        results = []
+
+        def traverse(current_node):
+            if current_node.left is not None:
+                traverse(current_node.left)
+            results.append(current_node.value)
+            if current_node.right is not None:
+                traverse(current_node.right)
+
+        traverse(self.root)
+        return results
+
 
 my_tree = BinarySearchTree()
 
@@ -73,3 +127,8 @@ print(my_tree.contains(18))
 print(my_tree.min_value_node(my_tree.root))
 
 print(my_tree.max_value_node(my_tree.root))
+
+print('bfs', my_tree.breadth_first_search())
+print('dfs-preorder', my_tree.depth_first_pre_order())
+print('dfs-postorder', my_tree.depth_first_post_order())
+print('dfs-inorder', my_tree.depth_first_inorder())
--- sort.py
+++ sort.py
@@ -1,3 +1,4 @@
+# Bubble Sort algorithm
 def bubble_sort(my_list):
     for i in range(len(my_list) - 1, 0, -1):
         for j in range(i):
@@ -12,6 +13,7 @@ my_list = [4, 3, 6, 5, 1, 2]
 print(bubble_sort(my_list))
 
 
+# Selection sort algorithm
 def selection_sort(my_list):
     for i in range(len(my_list) - 1):
         min_index = i
@@ -27,6 +29,8 @@ def selection_sort(my_list):
 
 print(selection_sort([4, 3, 6, 5, 1, 2]))
 
+# insertion sort algorithm
+
 
 def insertion_sort(my_list):
     for i in range(1, len(my_list)):
@@ -40,6 +44,9 @@ def insertion_sort(my_list):
 
 
 print(insertion_sort([4, 3, 6, 5, 1, 2]))
+
+# Merge sort algorithm
+# helper function for the merge sort
 
 
 def merge(list1, list2):
@@ -74,4 +81,44 @@ def merge_sort(my_list):
 print(merge_sort([4, 3, 6, 5, 1, 2, 9, 8, 7]))
 
 # O(n) for merge sort space complexity
-# O(n logn) for time complexity for merge sort
+# O(n log n) for time complexity for merge sort
+
+# Quick sort algorithm
+# helper function for quick sort
+# O(n log n) best and average case
+# worst case O(n^2) if already sorted data
+
+
+def swap(my_list, index1, index2):
+    temp = my_list[index1]
+    my_list[index1] = my_list[index2]
+    my_list[index2] = temp
+
+
+def pivot(my_list, pivot_index, end_index):
+    swap_index = pivot_index
+    for i in range(pivot_index+1, end_index+1):
+        if my_list[i] < my_list[pivot_index]:
+            swap_index += 1
+            swap(my_list, swap_index, i)
+    swap(my_list, pivot_index, swap_index)
+    return swap_index
+
+
+def quick_sort_helper(my_list, left, right):
+    if left < right:
+        pivot_index = pivot(my_list, left, right)
+        quick_sort_helper(my_list, left, pivot_index-1)
+        quick_sort_helper(my_list, pivot_index+1, right)
+    return my_list
+
+
+def quick_sort(my_list):
+    return quick_sort_helper(my_list, 0, len(my_list) - 1)
+
+
+quick_list = [4, 6, 1, 7, 3, 2, 5]
+print(pivot(quick_list, 0, 6))
+print(quick_list)
+
+print(quick_sort(quick_list))
